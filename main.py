306164242421883edf212d7ed0d33b2693e2781c
Ahmed-Silat <ahmedsilat95@gmail.com>
2026-03-18 00:26:31 +0500

    data cleaning layer implemented

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
+from itertools import tee
 
 from scripts.Extract import extract_csv_data
+from scripts.Transform import clean_data
 
 logging.basicConfig(
     filename = "logs/pipeline.log",
@@ -15,6 +17,10 @@ def run_pipeline():
     csv_data = extract_csv_data("data/users-etl-data.csv")
 
     print("Cleaning Data...")
+
+    cleaned = clean_data(csv_data)
+
+    csv_stream, db_stream = tee(cleaned)
 
 if __name__ == "__main__":
     run_pipeline()
